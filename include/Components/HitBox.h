73f7e7317fe14cf94d8ec8ff1613086261d7612f
Lý Tuấn Kiệt <lytuankiet2007@gmail.com>
2026-08-07 18:02:38 +0700

    Changed hitbox

--- include/Components/HitBox.h
+++ include/Components/HitBox.h
@@ -1,25 +1,38 @@
 #pragma once
 #include <Components/Faction.h>
 #include <Util/Sphere.h>
+#include <Util/Capsule.h>
 #include "raylib.h"
+#include <variant>
+#include <raymath.h>
 
 class HitBox {
 public:
-    HitBox(Sphere shape, float health_damage, float posture_damage, Faction faction, unsigned int attacker_id)
+    HitBox(std::variant<Sphere, Capsule> shape, float health_damage, float posture_damage, Faction faction, unsigned int attacker_id)
         : shape(shape), 
           health_damage(health_damage), 
           posture_damage(posture_damage), 
           faction(faction), 
           attacker_id(attacker_id) {}
 
-    Sphere getShape() const { return shape; }
+    bool isSphere() const { return std::holds_alternative<Sphere>(shape); }
+    bool isCapsule() const { return std::holds_alternative<Capsule>(shape); }
+
+    Sphere getSphere() const { return std::get<Sphere>(shape); }
+    Capsule getCapsule() const { return std::get<Capsule>(shape); }
+
+    Vector3 getCenter() const {
+        if (isSphere()) return getSphere().getCenter();
+        Capsule c = getCapsule();
+        return Vector3Scale(Vector3Add(c.getBase(), c.getTip()), 0.5f);
+    }
     float getHealthDamage() const { return health_damage; }
     float getPostureDamage() const { return posture_damage; }
     Faction getFaction() const { return faction; }
     unsigned int getAttackerId() const { return attacker_id; }
 
 private:
-    Sphere shape;
+    std::variant<Sphere, Capsule> shape;
     float health_damage;
     float posture_damage;
     Faction faction;
